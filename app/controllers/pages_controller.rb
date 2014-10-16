704bf40e7e183059544df323c22bc37a1517cb72
Krystal Flores <krystal.flores@gmail.com>
2014-10-16 14:21:46 -0700

    Made Sign in the homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,17 @@ class PagesController < ApplicationController
 
   def dashboard
 	end 
+
+	def resource_name
+:user
 end
+
+def resource
+@resource ||= User.new
+end
+
+def devise_mapping
+@devise_mapping ||= Devise.mappings[:user]
+end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "pages#home"
+  devise_scope :user do
+    authenticated :user do
+      root 'home#dashboard', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
   get "dashboard" => "pages#dashboard"
 
   # The priority is based upon order of creation: first created -> highest priority.
